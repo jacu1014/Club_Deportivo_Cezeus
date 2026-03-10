2c8d4087b684d367c9479084ab49bc535946ec9f
Alejandro Ceballos <49605039+jacu1014@users.noreply.github.com>
2026-03-10 09:49:43 -0500

    Update supabaseClient.js
    Se realiza actualizacion nuevamente en archivo Client

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -3,6 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+// Solo debe existir UN createClient en todo el proyecto
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
