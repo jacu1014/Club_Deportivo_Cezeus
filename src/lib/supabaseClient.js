b289bf11ea3a007f5308cc801177093ac7b6415f
Alejandro Ceballos <49605039+jacu1014@users.noreply.github.com>
2026-03-10 09:05:35 -0500

    Update supabaseClient.js
    Se agrega persistencia en el archivo supabaseClient

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,12 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Asegúrate de que no haya espacios en blanco en las variables
-export const supabase = createClient(supabaseUrl.trim(), supabaseAnonKey.trim())
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+});
