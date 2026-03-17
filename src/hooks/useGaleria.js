4749fe9ed5886a475b35e29b94fd92104b024af0
jacu1014 <jacu1014@gmail.com>
2026-03-16 21:16:00 -0500

    correcion en useGaleria

--- src/hooks/useGaleria.js
+++ src/hooks/useGaleria.js
@@ -5,7 +5,7 @@
 // Con una política RLS de SELECT pública.
 
 import { useState, useEffect } from 'react';
-import { supabase } from '../../../lib/supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 
 export function useGaleria() {
   const [fotos, setFotos]     = useState([]);
